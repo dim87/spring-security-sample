cb77324aefa029bb8434ec81b8a5a7a8c5768abd
Dmitry <latnikov@gmail.com>
2020-04-25 17:40:42 +0300

    Initial commit

---
+++ backend/src/main/java/com/sda/angularhomeworkbackend/AngularHomeworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sda.angularhomeworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularHomeworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularHomeworkBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sda/angularhomeworkbackend/AngularHomeworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.angularhomeworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularHomeworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
